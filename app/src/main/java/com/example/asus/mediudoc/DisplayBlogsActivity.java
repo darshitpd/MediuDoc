28d8af172310349158e0ccf4b41208c5ba08c80e
Darshit <darshitdoshi9@gmail.com>
2018-03-07 18:53:48 +0530

    Connection Working

--- app/src/main/java/com/example/asus/mediudoc/DisplayBlogsActivity.java
+++ app/src/main/java/com/example/asus/mediudoc/DisplayBlogsActivity.java
@@ -1,12 +1,20 @@
 package com.example.asus.mediudoc;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class DisplayBlogsActivity extends AppCompatActivity {
 
@@ -18,9 +26,82 @@ public class DisplayBlogsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_blogs);
 
         mBlogDatabase = FirebaseDatabase.getInstance().getReference().child("Feed");
-//
-//        mBlogList = (RecyclerView)findViewById(R.id.blog_list);
-//        mBlogList.setHasFixedSize(true);
-//        mBlogList.setLayoutManager(new LinearLayoutManager(this));
+
+        mBlogList = (RecyclerView)findViewById(R.id.blog_list);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<BlogPostSingle, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<BlogPostSingle, UsersViewHolder>(
+                BlogPostSingle.class, R.layout.single_blog_post, UsersViewHolder.class, mBlogDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(UsersViewHolder usersViewHolder, BlogPostSingle users, int i) {
+
+                usersViewHolder.setTitle(users.getTitle());
+                usersViewHolder.setAuthor(users.getAuthor());
+                usersViewHolder.setDate(users.getDate());
+
+                usersViewHolder.setUserImage(users.getImage(), getApplicationContext());
+
+                final String blog_id = getRef(i).getKey();
+
+                usersViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent profileIntent = new Intent(DisplayBlogsActivity.this, SingleBlogPostFull.class);
+                        profileIntent.putExtra("blog_id", blog_id);
+                        startActivity(profileIntent);
+                    }
+                });
+
+            }
+        };
+        mBlogList.setAdapter(firebaseRecyclerAdapter);
+    }
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+
+        }
+
+        public void setTitle(String title) {
+
+            TextView userNameView = (TextView) mView.findViewById(R.id.post_title);
+            userNameView.setText(title);
+
+        }
+
+        public void setDate(String date) {
+
+            TextView userDateView = (TextView) mView.findViewById(R.id.post_date);
+            userDateView.setText(date);
+
+        }
+
+        public void setAuthor(String author){
+
+            TextView userAuthorNameView = (TextView) mView.findViewById(R.id.post_author);
+            userAuthorNameView.setText(author);
+
+        }
+
+        public void setUserImage(String image, Context ctx){
+
+            ImageView userImageView = (ImageView) mView.findViewById(R.id.post_image);
+
+            Picasso.with(ctx).load(image).into(userImageView);
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/asus/mediudoc/SingleBlogPostFull.java
@@ -0,0 +1,60 @@
+package com.example.asus.mediudoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class SingleBlogPostFull extends AppCompatActivity {
+
+    private TextView mpost_title, mpost_date, mpost_author, mpost_description;
+    private ImageView mpost_image;
+
+    private DatabaseReference mBlogDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_blog_post_full);
+
+        mpost_author= (TextView)findViewById(R.id.post_author);
+        mpost_date=(TextView)findViewById(R.id.post_date);
+        mpost_title=(TextView)findViewById(R.id.post_title);
+        mpost_description= (TextView)findViewById(R.id.post_desc);
+        mpost_image =(ImageView)findViewById(R.id.post_image);
+
+        final String blog_id = getIntent().getStringExtra("blog_id");
+
+        mBlogDatabase= FirebaseDatabase.getInstance().getReference().child("Feed").child(blog_id);
+
+        mBlogDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String author = dataSnapshot.child("author").getValue().toString();
+                String title = dataSnapshot.child("title").getValue().toString();
+                String date = dataSnapshot.child("date").getValue().toString();
+                String description = dataSnapshot.child("desc").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                mpost_author.setText(author);
+                mpost_date.setText(date);
+                mpost_title.setText(title);
+                mpost_description.setText(description);
+                Picasso.with(SingleBlogPostFull.this).load(image).placeholder(R.drawable.default_avatar).into(mpost_image);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
